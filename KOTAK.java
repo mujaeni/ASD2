9383f101e397e65eea64678aa7ac0e7026ae3101
mujaeni <mujaeniahmad12@gmail.com>
2018-10-09 22:34:10 +0800

    Create KOTAK.java

---
+++ KOTAK.java
@@ -0,0 +1,54 @@
+public class KOTAK {
+	private double panjang, lebar, luas, keliling;
+
+	public double getPanjang() {
+		return panjang;
+	}
+
+	public double getLebar() {
+		return lebar;
+	}
+
+	public double getLuas() {
+		luas = panjang * lebar;
+		return luas;
+	}
+
+	public double getKeliling() {
+		keliling = panjang + lebar + panjang + lebar;
+		return keliling; 
+	}
+
+	public void setPanjang(double panjang) {
+		this.panjang = panjang;
+	}
+
+	public void setLebar(double lebar) {
+		this.lebar = lebar;
+	}
+
+	public void setLuas(double luas) {
+		this.luas = luas;
+	}
+
+	public void setKeliling(double keliling) {
+		this.keliling = keliling;
+	}
+
+}
+ @mujaeni
+   
+Write  Preview
+ 
+Leave a comment
+Choose Files Attach files by dragging & dropping, selecting them, or pasting from the clipboard.
+ Styling with Markdown is supported
+Comment
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
